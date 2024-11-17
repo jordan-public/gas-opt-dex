368922e93bc83c1adfca200ad02964b565ca6884
jordan <jordan@bitola.com>
2024-11-17 08:40:29 +0700

    Kinto, Bitkub

--- deployKinto.sh
+++ deployKinto.sh
@@ -6,4 +6,5 @@
 source .env
 
 # To deploy and verify our contract
-forge script script/GasPriceOptionsFactory.s.sol:Deploy --rpc-url "https://zircuit1-testnet.p2pify.com" --sender $SENDER --private-key $PRIVATE_KEY --broadcast -vvvv
+# forge script script/GasPriceOptionsFactory.s.sol:Deploy --rpc-url "https://42888.rpc.thirdweb.com/${THIRDWEB_API_KEY}" --sender $SENDER --private-key $PRIVATE_KEY --broadcast -vvvv
+forge script script/GasPriceOptionsFactory.s.sol:Deploy --rpc-url "http://35.215.120.180:8545" --sender $SENDER --private-key $PRIVATE_KEY --broadcast -vvvv
